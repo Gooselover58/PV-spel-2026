79a53233e384d1924b887da345e7a16ff8ce7875
Gooselover58 <alexander.ejeblad@elev.ga.lbs.se>
2026-01-13 22:21:27 +0100

    Added GrappleSetter particles

--- Assets/Scripts/Powerups/GrapplingSetter.cs
+++ Assets/Scripts/Powerups/GrapplingSetter.cs
@@ -24,6 +24,7 @@ public class GrapplingSetter : Powerup
         col.enabled = false;
         sr.color = deactiveColor;
         Global.playerGrappling.SetGrapples(setGrappleAmount);
+        EffectManager.Instance.PlayParticles("GrappleReset", transform.position, 15);
         CoolDown();
     }
 
