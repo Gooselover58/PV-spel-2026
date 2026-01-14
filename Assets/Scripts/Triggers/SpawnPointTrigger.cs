5e03998709f45df9a80d2417f520e0cc0ce2e0bc
Gooselover58 <alexander.ejeblad@elev.ga.lbs.se>
2026-01-14 10:29:39 +0100

    Saved Game scene

--- Assets/Scripts/Triggers/SpawnPointTrigger.cs
+++ Assets/Scripts/Triggers/SpawnPointTrigger.cs
@@ -6,11 +6,6 @@ public class SpawnPointTrigger : MonoBehaviour, ITrigger
 {
     [SerializeField] Vector3 spawnPointOffset;
 
-    private void Awake()
-    {
-        
-    }
-
     public void Triggered()
     {
         Global.respawnPoint = transform.position + spawnPointOffset;
