5764030014344801f878d76246f7399162146de2
Gooselover58 <alexander.ejeblad@elev.ga.lbs.se>
2026-01-30 10:59:29 +0100

    Adjusted Level 3

--- Assets/Scripts/Managers/main meny.cs
+++ Assets/Scripts/Managers/main meny.cs
@@ -7,17 +7,19 @@ using UnityEngine.SceneManagement;
 
 public class mainmeny : MonoBehaviour
 {
-    // Start is called before the first frame update
-   public void PlayGame()
+    public void PlayGame()
     {
-        //AudioManager.Instance.musicSource.Pause();
-        SceneManager.LoadScene(1);
-        
+        SceneManager.LoadScene(1);      
     }
+
     public void QuitGame()
     {
         Application.Quit();
         Debug.Log("Quit");
     }
 
+    public void PlayMenuSFX(string soundName)
+    {
+        AudioManager.Instance.PlaySFX(soundName);
+    }
 }
