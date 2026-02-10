224239958424af4dca805b9df2420a608a7b7b37
Gooselover58 <alexander.ejeblad@elev.ga.lbs.se>
2026-02-10 21:31:16 +0100

    More trailer things!!!

--- Assets/Scripts/SillyCamera.cs
+++ Assets/Scripts/SillyCamera.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.SceneManagement;
@@ -10,22 +11,42 @@ public class SillyCamera : MonoBehaviour
 
     [SerializeField] float sillyIncrease;
     [SerializeField] float sillySpeed;
+    [SerializeField] float sillyTextSpeed;
+    [SerializeField] TextMeshProUGUI dialogue;
 
     private void Awake()
     {
         volume = GetComponent<Volume>();
         volume.weight = 0;
+        dialogue.text = "";
     }
 
     private void Update()
     {
+        if (Input.GetKeyDown(KeyCode.R) && dialogue.transform.parent.gameObject.activeSelf)
+        {
+            StartCoroutine(DialogueThingy());
+        }
         if (Input.GetKeyDown(KeyCode.T))
         {
+            dialogue.transform.parent.gameObject.SetActive(false);
             volume.weight += sillyIncrease;
         }
-        if (Input.GetKeyDown(KeyCode.I))
+        if (Input.GetKeyDown(KeyCode.Y))
         {
             SceneManager.LoadScene("Game");
+        }
+    }
+
+    private IEnumerator DialogueThingy()
+    {
+        string dialogueText = "Revolutionary gameplay!!!";
+        string writtenText = "";
+        for (int i = 0; i < dialogueText.Length; i++)
+        {
+            writtenText += dialogueText[i];
+            dialogue.text = writtenText;
+            yield return new WaitForSeconds(sillyTextSpeed);
         }
     }
 }
